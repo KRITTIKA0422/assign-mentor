a03e394e37c4e9aac6f7b5ab30ea197be1ccdb4f
KRITTIKA0422 <krittika489podder@gmail.com>
2023-04-25 22:28:18 +0530

    gave unique names for each request

--- controllers/UserControllers.js
+++ controllers/UserControllers.js
@@ -1,30 +1,30 @@
 import { createstudents, listingstudents, creatementors, listingmentors, assignstudents, assignmentors, findboth } from "../models/UserModels.js";
 export const controller={
-    post: async function (request, response) {       //api endpoint for creating students
+    poststudents: async function (request, response) {       //api endpoint for creating students
         const data = request.body;
         console.log(data);
         const result = await createstudents(data);
         console.log(result);
         response.send(result);
     },
-    get: async function (request, response) {          //api endpoint for getting students
+    getstudents: async function (request, response) {          //api endpoint for getting students
         console.log(request.query);
         const students = await listingstudents(request);
         response.send(students);
     },
-    post: async function (request, response) {          //api endpoint for creating mentors
+    postmentors: async function (request, response) {          //api endpoint for creating mentors
         const data = request.body;
         console.log(data);
         const result = await creatementors(data);
         console.log(result);
         response.send(result);
     },
-    get: async function (request, response) {            //api endpoint for getting mentors
+    getmentors: async function (request, response) {            //api endpoint for getting mentors
         console.log(request.query);
         const mentors = await listingmentors(request);
         response.send(mentors);
     },
-    put: async function (request, response) {       //api endpoint for assigning multiple students under a mentor
+    putmultiplestudents: async function (request, response) {       //api endpoint for assigning multiple students under a mentor
         const { id } = request.params;
         const data = request.body;
         console.log(request.params, id);
@@ -32,7 +32,7 @@ export const controller={
         console.log(mentor);
         mentor? response.send(mentor) : response.send({ msg: "Mentor not found" });
     },
-    put: async function (request, response) {        //api endpoint for assigning a mentor to a student
+    putassignmentor: async function (request, response) {        //api endpoint for assigning a mentor to a student
         const { id } = request.params;
         const data = request.body;
         console.log(request.params, id);
@@ -40,7 +40,7 @@ export const controller={
         console.log(student);
         student? response.send(student) : response.send({ msg: "Student not found" });
     },
-    get: async function (request, response) {        //api endpoint for viewing all students under a mentor
+    getboth: async function (request, response) {        //api endpoint for viewing all students under a mentor
         const { id } = request.params;
         console.log(request.params, id);
         const result=await findboth(id);
